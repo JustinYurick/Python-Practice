5b086e49ad093aab436fbebee54fb8f913a94d6e
Justin Yurick <65051130+JustinYurick@users.noreply.github.com>
2022-11-27 17:48:48 -0500

    added dictonary code and type casting

--- pratice.py
+++ pratice.py
@@ -8,12 +8,12 @@ iterating_num = 1
 print('Hello my name is', name)
 
 #to revrse a string we can use [::-1]
-print('My name backwards is', name[::-1])
+print('\nMy name backwards is', name[::-1])
 
 if is_locked:
-    print("It is locked")
+    print("\nIt is locked")
 else:
-    print('It is unlocked')
+    print('\nIt is unlocked')
 
 for num in num_list:
     print(num)
@@ -30,7 +30,42 @@ def sumNum(num1 , num2):
 def sumNumReturn(num1, num2):
     return num1 * num2
 
-print('Two numbers sumed using print')
+#default values can be given in funtions
+def sumWithDefaults(num1 = 1, num2 = 1):
+    return num1 * num2
+
+print('\nTwo numbers multipled using print')
 sumNum(2,4)
 
-print('Two nums sumed using return', sumNumReturn(2,4))
+print('\nTwo nums multipled using return', sumNumReturn(2,4))
+
+print('\nTwo nums multipled using default parameters ', sumWithDefaults())
+
+#creating basic dictonary
+contact_Info = { 'Matt' : '400-223-4124',
+                 'Greg' : '213-234-1678',
+                 'Rachel' : '558-465-5462'}
+
+#how to access values
+print('\n',contact_Info['Matt'])
+
+#deleting key-value pairs
+del(contact_Info['Matt'])
+for contact in contact_Info:
+    print('\n', contact)
+
+#adding key-value pairs and also printing both values in a for loop
+contact_Info['Matthew'] = '400-223-4124'
+for name, phoneNum in contact_Info.items():
+    print('\n',name, ':', phoneNum)
+
+#dictonary with a list
+numbers = { 'Odd' : [1,3,5,7,9],
+            'Even' : [2,4,6,8,10],
+            'Prime' : [2,3,5,7]}
+print('\n The first prime number is', numbers['Prime'][0])
+
+#casting string to int
+print('\nA string casted into and int', int('100'))
+print('\nA int casted into and string', str(100))
+print('\nA float casted into and int', int(2.3))
