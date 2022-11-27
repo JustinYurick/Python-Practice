e26f840539b7129e638c32698db77e1f8ad45db2
Justin Yurick <65051130+JustinYurick@users.noreply.github.com>
2022-11-27 17:10:14 -0500

    Added some comments to the coded

--- pratice.py
+++ pratice.py
@@ -4,7 +4,10 @@ num_list = [115,86,44,31]
 iterating_num = 1
 
 
+#simple print statement
 print('Hello my name is', name)
+
+#to revrse a string we can use [::-1]
 print('My name backwards is', name[::-1])
 
 if is_locked:
@@ -19,9 +22,11 @@ while iterating_num<= 10:
     print(iterating_num)
     iterating_num = iterating_num + 1
 
+#a funtion that doesnt return anything
 def sumNum(num1 , num2):
     print(num1 * num2)
 
+#a function that does return something
 def sumNumReturn(num1, num2):
     return num1 * num2
 
